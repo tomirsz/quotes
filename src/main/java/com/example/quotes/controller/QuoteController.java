2741ee3d7293d88146d8dce7d11df44ad92ce601
tomirsz <tomirsz@o2.pl>
2021-08-22 14:29:02 +0200

    Add healtcheck

--- src/main/java/com/example/quotes/controller/QuoteController.java
+++ src/main/java/com/example/quotes/controller/QuoteController.java
@@ -4,16 +4,19 @@ import com.example.quotes.model.Quote;
 import com.example.quotes.service.QuoteService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/quote")
 public class QuoteController {
 
     private final QuoteService quoteService;
 
-    @GetMapping("/random")
+    @GetMapping("/quote/random")
     public Quote getRandom() { return quoteService.getRandomQuote(); }
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "OK";
+    }
 }
